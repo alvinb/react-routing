6f0b3d4bb9e51b4037808aa3ad73b6da1bb81251
Alvin Bundalian <alvin.bundalian@telescope.tv>
2020-05-31 17:33:36 -0700

    initial commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.css';
+export function Footer(){
+  return (
+    <>
+      <footer className='footer'>
+        <ul>
+          <li>Privacy</li>
+          <li>Terms</li>
+          <li>About</li>
+        </ul>
+      </footer>
+    </>
+  );
+}
